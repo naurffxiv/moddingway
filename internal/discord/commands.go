57dc827f6a3af18fadc558f2d4296a4a0c5cfe32
OTCompa <41562938+OTCompa@users.noreply.github.com>
2024-05-30 05:31:49 -0500

    implement basic kick functionality

--- internal/discord/commands.go
+++ internal/discord/commands.go
@@ -1,11 +1,36 @@
 package discord
 
 import (
+	"fmt"
+
 	"github.com/bwmarrin/discordgo"
 )
 
 func (d *Discord) Kick(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	options := i.ApplicationCommandData().Options
+	optionMap := make(map[string]*discordgo.ApplicationCommandInteractionDataOption, len(options))
+	for _, opt := range options {
+		optionMap[opt.Name] = opt
+	}
 
+	userToKick := optionMap["user"].UserValue(nil).ID
+
+	var err error
+	if optionMap["reason"] != nil {
+		err = d.Session.GuildMemberDeleteWithReason(i.GuildID, userToKick, optionMap["reason"].StringValue())
+	} else {
+		err = d.Session.GuildMemberDelete(i.GuildID, userToKick)
+	}
+
+	if err != nil {
+		tempstr := fmt.Sprintf("Could not kick user <@%v>", userToKick)
+		fmt.Printf("%v: %v\n", tempstr, err)
+		StartInteraction(s, i.Interaction, tempstr)
+	} else {
+		tempstr := fmt.Sprintf("User <@%v> has been kicked.", userToKick)
+		fmt.Printf("%v\n", tempstr)
+		StartInteraction(s, i.Interaction, tempstr)
+	}
 }
 
 func (d *Discord) Mute(s *discordgo.Session, i *discordgo.InteractionCreate) {
