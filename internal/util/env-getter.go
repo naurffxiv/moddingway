70e941bcbe88ded50c1147eda008531866d26af0
OTCompa <41562938+OTCompa@users.noreply.github.com>
2024-07-23 00:11:16 -0500

    make getting env vars easier

---
+++ internal/util/env-getter.go
@@ -0,0 +1,24 @@
+package util
+
+import (
+	"os"
+)
+
+// EnvGetter is a helper that removes the need for error checking
+// every single an environment variable is looked up
+// instead just moves error checking to the end of the block of code
+
+type EnvGetter struct {
+	EnvName string
+	Ok      bool
+}
+
+func (eg *EnvGetter) GetEnv(envName string) string {
+	if !eg.Ok {
+		return ""
+	}
+	eg.EnvName = envName
+	var ret string
+	ret, eg.Ok = os.LookupEnv(envName)
+	return ret
+}
