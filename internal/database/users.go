f997188876cdee7c69184d917940803f17d69213
OTCompa <41562938+OTCompa@users.noreply.github.com>
2024-08-06 16:41:31 -0500

    split AddOrGetExistingUser into two

--- internal/database/users.go
+++ internal/database/users.go
@@ -6,21 +6,9 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-// AddOrGetExistingUser returns the userID in the users table of the database.
-// If the user does not exist in the table, it will add a new entry and return the userId
-// If the user already exists, it will just return the existing userId
-func AddOrGetExistingUser(conn *pgxpool.Pool, discordUserID string, discordGuildID string) (int, error) {
-	query := `WITH ret AS (
-		INSERT INTO users (discordUserId, discordGuildId, ismod)
-		VALUES ($1, $2, false)
-		ON CONFLICT (discordUserId, discordGuildId) DO NOTHING
-		RETURNING userID
-	)
-	SELECT userID FROM ret
-	UNION ALL
-	SELECT userID FROM users
-	WHERE discordUserId = $1 AND discordGuildId = $2
-	LIMIT 1`
+func GetUser(conn *pgxpool.Pool, discordUserID string, discordGuildID string) (int, error) {
+	query := `SELECT userID FROM users
+	WHERE discordUserId = $1 AND discordGuildID = $2`
 
 	var dbUserID int
 	err := conn.QueryRow(context.Background(), query, discordUserID, discordGuildID).Scan(&dbUserID)
@@ -30,10 +18,11 @@ func AddOrGetExistingUser(conn *pgxpool.Pool, discordUserID string, discordGuild
 	return dbUserID, nil
 }
 
-func GetExistingUser(conn *pgxpool.Pool, discordUserID string, discordGuildID string) (int, error) {
-	query := `SELECT userID FROM users
-	WHERE discordUserId = $1 AND discordGuildID = $2`
-
+func AddUser(conn *pgxpool.Pool, discordUserID string, discordGuildID string) (int, error) {
+	query := `INSERT INTO users (discordUserId, discordGuildId, ismod)
+	VALUES ($1, $2, false)
+	ON CONFLICT (discordUserId, discordGuildId) DO NOTHING
+	RETURNING userID`
 	var dbUserID int
 	err := conn.QueryRow(context.Background(), query, discordUserID, discordGuildID).Scan(&dbUserID)
 	if err != nil {
