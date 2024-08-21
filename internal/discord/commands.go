7b53e71faeb257ec318d2639b0c2cec25d0ae347
Maheen K <99596115+MachoMaheen@users.noreply.github.com>
2024-08-21 09:18:47 +0530

    Update commands.go after removing the methods associated with /slowmode , /slowmodeoff ,/kick commands.
    Updated commands.go as per the issue #84

--- internal/discord/commands.go
+++ internal/discord/commands.go
@@ -14,9 +14,9 @@ import (
 //
 //	user: 	User
 //	reason: string
-func (d *Discord) Kick(s *discordgo.Session, i *discordgo.InteractionCreate) {
+// func (d *Discord) Kick(s *discordgo.Session, i *discordgo.InteractionCreate) {
 
-}
+// }
 
 // Mute attempts to mute the user specified user from the server the command was invoked in.
 // Fields:
@@ -134,14 +134,14 @@ func (d *Discord) SetNickname(s *discordgo.Session, i *discordgo.InteractionCrea
 // Fields:
 //
 //	duration:	string
-func (d *Discord) Slowmode(s *discordgo.Session, i *discordgo.InteractionCreate) {
+// func (d *Discord) Slowmode(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	
-}
+// }
 
 // SlowmodeOff attempts to remove slowmode from the current channel.
-func (d *Discord) SlowmodeOff(s *discordgo.Session, i *discordgo.InteractionCreate) {
+// func (d *Discord) SlowmodeOff(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	
-}
+// }
 
 // Purge attempts to remove the last message-number messages from the specified channel.
 // Fields:
