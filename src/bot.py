9dd99b0fb163eba42eb0b25dd16dd0ccfc0c923e
OTCompa <41562938+OTCompa@users.noreply.github.com>
2024-10-17 11:30:44 -0500

    change tasks to workers for clarity

--- src/bot.py
+++ src/bot.py
@@ -4,7 +4,7 @@ from settings import get_settings
 from commands.exile_commands import create_exile_commands
 from commands.ban_commands import create_ban_commands
 import logging
-import tasks
+import workers
 
 settings = get_settings()
 logger = logging.getLogger(__name__)
@@ -20,7 +20,7 @@ class ModdingwayBot(Bot):
 
     async def on_ready(self):
         logger.info(f"Logged in as {self.user} (ID: {self.user.id})")
-        tasks.start_tasks(self)
+        workers.start_tasks(self)
 
     def _register_commands(self):
         create_exile_commands(self)
