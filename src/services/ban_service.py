0c19301da62fb552c9b989beaf1b93083be83060
Alex Gideon <alexandergideon2002@gmail.com>
2024-11-04 19:10:10 -0600

    reformatted w/ black formatter.

--- src/services/ban_service.py
+++ src/services/ban_service.py
@@ -1,6 +1,6 @@
 import discord
 import logging
-from typing import Optional,Tuple
+from typing import Optional, Tuple
 from util import log_info_and_embed, send_dm
 from settings import get_settings
 
@@ -9,8 +9,7 @@ logger = logging.getLogger(__name__)
 
 
 async def ban_user(
-    user: discord.Member, 
-    reason: str
+    user: discord.Member, reason: str
 ) -> Optional[Tuple[bool, bool, str]]:
     """Executes ban of user
 
@@ -24,16 +23,15 @@ async def ban_user(
             - dm_state (bool): Indicates whether the DM notification to the user succeeded.
             - result_description (str): A message describing the status of the ban operation.
     """
-    if len(reason) < 512: 
-        
+    if len(reason) < 512:
         result_description = ""
         dm_state = False
-        #attempt to send a DM to the user with the reason for the ban
+        # attempt to send a DM to the user with the reason for the ban
         try:
             await send_dm(
                 user,
                 f"You are being banned from NA Ultimate Raiding - FF XIV for the following reason: \n> {reason} "
-                "\nYou may appeal this ban by contacting the moderators of the server in 30 days."
+                "\nYou may appeal this ban by contacting the moderators of the server in 30 days.",
             )
             logger.info(f"Successfully sent dm to {user.mention}")
             dm_state = True
@@ -47,7 +45,7 @@ async def ban_user(
             logger.error(f"DM to {user.mention} due to Unknown Error: {e}")
             dm_state = False
 
-        #attempt Ban
+        # attempt Ban
         try:
             await user.ban(reason=reason)
             logger.info(f"Successfully banned {user.mention}")
@@ -61,21 +59,21 @@ async def ban_user(
         except Exception as e:
             logger.error(f"Ban of {user.mention} failed due to Unknown Error: {e}")
             result = False
-    
-        if not result and dm_state: #ban fail dm succeed.
-            result_description =  f"Unable to ban {user.mention}, please ban via discord built-in ban feature. A DM has been sent to {user.mention} with ban reason."
-        elif result and not dm_state: #ban succeed dm fail.
-            result_description = f"Successfully banned {user.mention} but DM failed to send."
-        elif result and dm_state: #full success
+
+        if not result and dm_state:  # ban fail dm succeed.
+            result_description = f"Unable to ban {user.mention}, please ban via discord built-in ban feature. A DM has been sent to {user.mention} with ban reason."
+        elif result and not dm_state:  # ban succeed dm fail.
+            result_description = (
+                f"Successfully banned {user.mention} but DM failed to send."
+            )
+        elif result and dm_state:  # full success
             result_description = f"Successfully banned {user.mention} and DM has been sent with ban reason."
-        else: #full failure
+        else:  # full failure
             result_description = f"Unable to ban {user.mention} and unable to send DM. Please ban via discord built-in ban feature or try again later."
 
     else:
-        #reason too large, ban action canceled.
+        # reason too large, ban action canceled.
         result_description = f"Unable to ban: {user.mention}, reason given is too long (above 512 characters). Please shorten ban reason."
         result = False
-    #always return this.    
-    return (result,dm_state,result_description)
-        
-    
+    # always return this.
+    return (result, dm_state, result_description)
