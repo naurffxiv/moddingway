2f173d756cf41b538ce26c6c6cb8a70d535eefb4
Claire <nicholas.mellinger@outlook.com>
2024-11-24 16:31:30 -0500

    Update helper.py

--- src/commands/helper.py
+++ src/commands/helper.py
@@ -21,7 +21,10 @@ def create_logging_embed(interaction: discord.Interaction, **kwargs):
                 case discord.ChannelType:
                     fields.append(EmbedField(key.title(), f"<#{value}>"))
                 case _:
-                    fields.append(EmbedField(key.title(), value))
+                    if "delete_messages_flag" in key:
+                        fields.append(EmbedField("Messages Deleted", value))
+                    else:
+                        fields.append(EmbedField(key.title(), value))
 
     return create_interaction_embed_context(
         interaction.guild.get_channel(settings.logging_channel_id),
