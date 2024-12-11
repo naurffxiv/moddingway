ee612b681218464cbf0c937d7e6170031a708971
KingPieson <crazedsportsfan@gmail.com>
2024-12-11 18:11:49 -0500

    Add initial few exile service tests

---
+++ tests/unit/services/test_exile_service.py
@@ -0,0 +1,47 @@
+from pytest_mock.plugin import MockerFixture
+
+from moddingway.database.models import User
+from moddingway.services import exile_service
+from datetime import timedelta
+
+
+async def test_exile_user__unverified(mocker: MockerFixture):
+    # Arrange
+    mocker.patch("moddingway.util.user_has_role", return_value=False)
+    mocked_member = mocker.Mock()
+
+    # Act
+    res = await exile_service.exile_user(
+        mocker.Mock(), mocked_member, timedelta(days=1), "test_exile_user__unverified"
+    )
+
+    # Assert
+    assert res is not None
+    assert res == "User is not currently verified, no action will be taken"
+
+
+async def test_exile_user__verified_existing_user_dm_failed(mocker: MockerFixture, create_member):
+    # Arrange
+    mock_database_user = User(
+        user_id=1,
+        discord_user_id="12345",
+        discord_guild_id="1",
+        is_mod=False,
+        temporary_points=0,
+        permanent_points=0,
+    )
+    mocker.patch("moddingway.database.users_database.get_user", return_value=mock_database_user)
+    mocker.patch("moddingway.database.exiles_database.add_exile", return_value=4001)
+
+    mocked_member = create_member()
+
+    # Act
+    res = await exile_service.exile_user(
+        mocker.Mock(),
+        mocked_member,
+        timedelta(days=1),
+        "test_exile_user verified existing_user dm_failed",
+    )
+
+    # Assert
+    assert res is None
