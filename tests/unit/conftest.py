38d6be0686d2f5639f670723ebae0966b897112c
KingPieson <crazedsportsfan@gmail.com>
2024-12-11 20:32:48 -0500

    remove patch mocks for util methods

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -44,7 +44,7 @@ def naur_guild(mocker: MockerFixture, create_role):
 
 @pytest.fixture
 def create_member(mocker: MockerFixture, naur_guild, create_role):
-    def __create_member(roles: List[enums.Role] = [enums.Role.VERIFIED]):
+    def __create_member(roles: List[enums.Role] = [enums.Role.VERIFIED], allows_dms: bool = True):
         role_list = [create_role(role) for role in roles]
         mocked_member = mocker.Mock(
             guild=naur_guild,
@@ -52,6 +52,10 @@ def create_member(mocker: MockerFixture, naur_guild, create_role):
             add_roles=mocker.AsyncMock(),
             remove_roles=mocker.AsyncMock(),
         )
+
+        mocked_member.create_dm = mocker.AsyncMock()
+        if not allows_dms:
+            mocked_member.create_dm.side_effect = Exception("Cannot create DM")
 
         return mocked_member
     return __create_member
--- tests/unit/services/test_exile_service.py
+++ tests/unit/services/test_exile_service.py
@@ -20,7 +20,9 @@ async def test_exile_user__unverified(mocker: MockerFixture, create_member):
     assert res == "User is not currently verified, no action will be taken"
 
 
-async def test_exile_user__verified_existing_user_dm_failed(mocker: MockerFixture, create_member):
+async def test_exile_user__verified_existing_user_dm_failed(
+    mocker: MockerFixture, create_member
+):
     # Arrange
     mock_database_user = User(
         user_id=1,
@@ -30,14 +32,21 @@ async def test_exile_user__verified_existing_user_dm_failed(mocker: MockerFixtur
         temporary_points=0,
         permanent_points=0,
     )
-    mocker.patch("moddingway.database.users_database.get_user", return_value=mock_database_user)
-    mocker.patch("moddingway.database.exiles_database.add_exile", return_value=4001)
+    mocker.patch(
+        "moddingway.database.users_database.get_user", return_value=mock_database_user
+    )
+    create_user_mock = mocker.patch(
+        "moddingway.database.users_database.add_user", return_value=mock_database_user
+    )
+    exile_id = 4001
+    mocker.patch("moddingway.database.exiles_database.add_exile", return_value=exile_id)
 
-    mocked_member = create_member(roles=[enums.Role.VERIFIED])
+    mocked_member = create_member(roles=[enums.Role.VERIFIED], allows_dms=False)
+    mocked_logging_embed = mocker.Mock()
 
     # Act
     res = await exile_service.exile_user(
-        mocker.Mock(),
+        mocked_logging_embed,
         mocked_member,
         timedelta(days=1),
         "test_exile_user verified existing_user dm_failed",
@@ -45,3 +54,11 @@ async def test_exile_user__verified_existing_user_dm_failed(mocker: MockerFixtur
 
     # Assert
     assert res is None
+    create_user_mock.assert_not_called()
+    mocked_logging_embed.set_footer.assert_called_with(text=f"Exile ID: {exile_id}")
+
+    assert any(
+        call[1].get("name", "") == "DM Status"
+        for call in mocked_logging_embed.add_field.call_args_list
+    )
+    
