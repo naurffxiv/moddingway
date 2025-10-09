b377f4fc56e465d3d00f9a85da33c339eff96836
Dale Karp <obsoke@obsoke.com>
2025-10-09 16:05:40 -0400

    feat(strike): list_strikes now displays created_at

--- tests/unit/services/test_strike_service.py
+++ tests/unit/services/test_strike_service.py
@@ -5,7 +5,7 @@ from moddingway.services import strike_service, ban_service, exile_service
 from moddingway import constants
 from typing import List
 import discord
-from datetime import timedelta
+from datetime import datetime, timedelta
 
 
 @pytest.mark.parametrize(
@@ -72,21 +72,33 @@ async def test_apply_punisment(
         (
             "1",
             "1",
-            [("1", constants.StrikeSeverity.MINOR, "test", "1")],
+            [("1", constants.StrikeSeverity.MINOR, "test", "1", datetime(2025, 9, 1))],
             "Strikes found for <@1>: [Temporary points: None | Permanent points: None]"
-            "\n* ID: 1 | SEVERITY: 1 | Moderator: <@1> | REASON: test"
+            "\n* ID: 1 | SEVERITY: 1 | Moderator: <@1> | START DATE: <t:1756699200:F> | REASON: test"
             "\nTotal Points: 1",
         ),
         (
             "1",
             "3",
             [
-                ("1", constants.StrikeSeverity.MINOR, "test", "1"),
-                ("2", constants.StrikeSeverity.MODERATE, "test2", "3"),
+                (
+                    "1",
+                    constants.StrikeSeverity.MINOR,
+                    "test",
+                    "1",
+                    datetime(2025, 9, 1),
+                ),
+                (
+                    "2",
+                    constants.StrikeSeverity.MODERATE,
+                    "test2",
+                    "3",
+                    datetime(2025, 9, 2),
+                ),
             ],
             "Strikes found for <@1>: [Temporary points: None | Permanent points: None]"
-            "\n* ID: 1 | SEVERITY: 1 | Moderator: <@1> | REASON: test"
-            "\n* ID: 2 | SEVERITY: 2 | Moderator: <@3> | REASON: test2"
+            "\n* ID: 1 | SEVERITY: 1 | Moderator: <@1> | START DATE: <t:1756699200:F> | REASON: test"
+            "\n* ID: 2 | SEVERITY: 2 | Moderator: <@3> | START DATE: <t:1756785600:F> | REASON: test2"
             "\nTotal Points: 3",
         ),
     ],
@@ -116,7 +128,6 @@ async def test_get_user_strikes(
 async def test_add_strike(
     create_db_user, create_member, create_embed, mocker: MockerFixture
 ):
-
     mocked_db_user = create_db_user(user_id=1, temporary_points=0, get_strike_points=0)
 
     mocked_user = create_member(id=1)
